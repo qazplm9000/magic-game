17304d2dce695e145081dd82b948619745ac8e4e
qazplm9000 <qazplm9000@users.noreply.github.com>
2020-07-28 16:12:06 -0700

    Flag System Started
    Started a flag system
    FlagManager Editor unfinished, can't compile

---
+++ Magic World/Assets/Scripts/Combatant/Flag System/Editor/FlagManagerEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class FlagManagerEditor : Editor
+{
+    FlagManager fm;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        fm = (FlagManager)target;
+
+        EditorGUILayout.Foldout(fm.foldout, "Flags");
+
+        EditorGUI.indentLevel++;
+        FlagGUI();
+        EditorGUI.indentLevel--;
+
+        EditorUtility.SetDirty(target);
+    }
+
+    private void FlagGUI()
+    {
+        Flag[] flagEnum = (Flag[])Enum.GetValues(typeof(Flag));
+        for (int i = 0; i < fm.flagValues.Count; i++)
+        {
+            EditorGUI.Toggle()
+        }
+    }
+}
---
+++ Magic World/Assets/Scripts/Combatant/Flag System/FlagManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum Flag
+{
+    character_is_moving,
+    character_can_move,
+    character_is_casting,
+    character_can_cast
+}
+
+public class FlagManager : MonoBehaviour, ISerializationCallbackReceiver
+{
+    public bool foldout;
+    public List<bool> flagValues = new List<bool>();
+
+
+    private void Awake()
+    {
+        
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void OnAfterDeserialize()
+    {
+        
+    }
+
+    public void OnBeforeSerialize()
+    {
+        int numOfFlags = Enum.GetNames(typeof(Flag)).Length;
+        if (numOfFlags != flagValues.Count)
+        {
+            flagValues = new List<bool>(numOfFlags);
+            bool[] tempFlags = new bool[numOfFlags];
+            flagValues.AddRange(tempFlags);
+        }
+    }
+}
